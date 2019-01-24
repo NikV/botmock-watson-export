682beab1ac6d382b77decbf524cbd0197eb8c3d3
Kevin Donahue <nonnontrivial@gmail.com>
2019-01-24 08:11:44 -0500

    fix sibling bug in createDialogNodes

--- Provider.js
+++ Provider.js
@@ -12,24 +12,25 @@ module.exports = class Provider {
     const vs = await this.client.variables(BOTMOCK_TEAM_ID, BOTMOCK_PROJECT_ID);
     const context = vs.reduce((acc, v) => ({ ...acc, [v.name]: v.default_value }), {});
     const nodes = [];
-    let i;
-    let siblings = [];
+    const siblingMap = {};
     const { board } = await this.client.boards(
       BOTMOCK_TEAM_ID,
       BOTMOCK_PROJECT_ID,
       BOTMOCK_BOARD_ID
     );
+    let i;
     for (const x of board.messages) {
+      const [prev = {}] = x.previous_message_ids;
       // We need to hold on to siblings so that we can define a `previous_sibling`
       // from the perspective of another node.
       if (x.next_message_ids.length > 1) {
-        siblings = x.next_message_ids.map(m => m.message_id);
+        siblingMap[x.message_id] = x.next_message_ids.map(m => m.message_id);
       }
       let previous_sibling;
+      const siblings = siblingMap[prev.message_id] || [];
       if ((i = siblings.findIndex(s => x.message_id === s))) {
         previous_sibling = siblings[i - 1];
       }
-      const [prev = {}] = x.previous_message_ids;
       nodes.push({
         title: x.payload.nodeName ? x.payload.nodeName.replace(/\s/g, '-') : '_',
         previous_sibling,
